e23d7542e35fc8f22a8f5d7238c198c1e9c515c3
Justin Brehm <jbrehm@reverbnation.com>
2013-03-28 15:02:17 -0400

    Made router_id and attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,6 +10,7 @@ default[:keepalived][:notification_emails] = []
 default[:keepalived][:notification_email_from] = "root@localhost"
 default[:keepalived][:smtp_server] = nil
 default[:keepalived][:smtp_connect_timeout] = nil
+default[:keepalived][:router_id] = "LVS_DEVEL"
 
 default[:keepalived][:vrrp_instances] = [
   # define a hash for each vrrp_instance:
